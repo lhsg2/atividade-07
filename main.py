323a57a5a8adbd558efa5aad5e0408515790ee42
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-06-22 12:08:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+metros2 = float(input("degite os metros quadrados a ser pintado:"))
+litros = metros2/3
+lata = litros/18
+preco = lata*80
+print(f"latas que sera usadas:{lata:.1f}")
+print(f"preço total das latas:${preco:.2f}")
