f7730efe3453fd2f6ef58e3822bcc4e263465f40
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-06-22 12:16:35 +0000

    correção se der menos de 1 lata

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-metros2 = float(input("degite os metros quadrados a ser pintado:"))
-litros = metros2/3
+metros = float(input("degite os metros quadrados a ser pintado:"))
+litros = metros/3
 lata = litros/18
 preco = lata*80
-print(f"latas que sera usadas:{lata:.1f}")
-print(f"preço total das latas:${preco:.2f}")
+if lata > 1:
+  print(f"latas que sera usadas:{lata:.1f}")
+  print(f"preço total das latas:${preco:.2f}")
+else:
+  print(f"sera usado {lata:.1f}% de uma lata")
+  print(f"preço da lata é:$80")
